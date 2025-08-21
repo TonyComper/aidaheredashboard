5b57b9354288aaa61add9a55798d22f8fd701cf1
TonyComper <tonythec@gmail.com>
2025-08-21 16:48:11 -0400

    Allow deploy with lint errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Allow builds to succeed even if ESLint finds errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
